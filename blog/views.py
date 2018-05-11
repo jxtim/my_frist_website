81afec6009b2a23616f97b4607d4ff817c69b53b
alex <949114543@qq.com>
2018-05-11 12:14:49 +0800

    tim

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from . models import Comment
 def home_page(request):
-	if request.method == "POST":
-		comment = request.POST.get("comment")
-		user = request.POST.get('username')
-		return render(request, 'blog/home_page.html', {'posted_comment': comment})
-		
-	if request.method == "GET":
-		return render(request, 'blog/home_page.html')
+    if request.method == 'POST':
+        our_comment = request.POST.get('comment')
+        our_user = request.POST.get("username")
+        comment_obj = Comment.objects.create(
+            comment=our_comment,
+            user=our_user
+        )
+
+        return render(request, 'blog/home_page.html', {'comment': comment_obj,})
+    return render(request, 'blog/home_page.html')
